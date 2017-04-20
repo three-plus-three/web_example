dc74cf831b0b97a128f121b23a75d28368dbd5c0
runner.mei <runner.mei@gmail.com>
2017-04-20 19:55:36 +0800

    rename gengen

--- gengen_build.bat
+++ gengen_build.bat
@@ -1,10 +1,10 @@
 
 if "%gengen_path%" EQU "" (
-  if exist D:\developing\go\meijing\tpt_vendor\src\github.com\runner-mei\gengen (
-    set gengen_path=D:\developing\go\meijing\tpt_vendor\src\github.com\runner-mei\gengen
+  if exist D:\developing\go\meijing\tpt_vendor\src\github.com\three-plus-three\gengen (
+    set gengen_path=D:\developing\go\meijing\tpt_vendor\src\github.com\three-plus-three\gengen
   )
-  if exist D:\dev\tpt_vendor\src\github.com\runner-mei\gengen (
-    set gengen_path=D:\dev\tpt_vendor\src\github.com\runner-mei\gengen
+  if exist D:\dev\tpt_vendor\src\github.com\three-plus-three\gengen (
+    set gengen_path=D:\dev\tpt_vendor\src\github.com\three-plus-three\gengen
   )
 )
 
